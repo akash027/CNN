fe22c14e637cfad56f715c0bea02aca4372ad8cb
akash027 <33549683+akash027@users.noreply.github.com>
2020-04-05 22:36:46 +0530

    Update cifar-model & pred.py

--- cifar image classification/cifar-model & pred.py
+++ cifar image classification/cifar-model & pred.py
@@ -84,8 +84,66 @@ history = model.fit(x_train,y_train,
                     validation_data = (x_test,y_test),
                     shuffle=True,verbose=1)
 
-model.save('/home/sky/Documents/cifar10_model.h5')
+model.save('/cifar10_model.h5')
 
 scores = model.evaluate(x_test, y_test, verbose=1)
 print("'Test loss: ", scores[0])
 print("'Test Accuracy: ", scores[1])
+
+## Prediction
+import cv2
+import numpy as np
+from keras.models import load_model
+
+img_row, img_height, img_depth = 32,32,3
+
+classifier = load_model('/cifar10_model.h5')
+
+color = True
+scale = 8
+
+
+def draw_test(name, res, input_im, scale, img_row, img_height):
+    BLACK = [0,0,0]
+    res = int(res)
+    if res == 0:
+        pred = 'airoplane'
+    if res == 1:
+        pred = 'automobile'
+    if res == 2:
+        pred = 'bird'
+    if res == 3:
+        pred = 'cat'
+    if res == 4:
+        pred = 'deer'
+    if res == 5:
+        pred = 'dog'
+    if res == 6:
+        pred = 'frog'
+    if res == 7:
+        pred = 'horse'
+    if res == 8:
+        pred = 'ship'
+    if res == 9:
+        pred = 'truck'
+    
+    expanded_image = cv2.copyMakeBorder(input_im, 0, 0, 0, imageL.shape[0]*2, cv2.BORDER_CONSTANT,value=BLACK)
+    if color == False:
+        expanded_image = cv2.cvtColor(expanded_image, cv2.COLOR_BGR2GRAY)
+    cv2.putText(expanded_image, str(pred), (300, 80), cv2.FONT_HERSHEY_COMPLEX_SMALL, 3, (0,255,0), 2)
+    cv2.imshow(name, expanded_image)
+
+for i in range(0,10):
+    rand = np.random.randint(0,10000)
+    input_im = x_test[rand]
+    
+    imageL = cv2.resize(input_im, None, fx=4, fy=4, interpolation = cv2.INTER_CUBIC)
+    input_im = input_im.reshape(1,img_row,img_height,img_depth)
+    
+    #Get prediction
+    res = str(classifier.predict_classes(input_im, 1, verbose=0)[0])
+    
+    draw_test("prediction",res, imageL, scale, img_row, img_height)
+    cv2.waitKey(0)
+
+cv2.destroyAllWindows()
