c79a311649a0fca09feec084d3ff271674d463f8
akash027 <33549683+akash027@users.noreply.github.com>
2020-04-10 00:01:44 +0530

    Add files via upload

---
+++ Emotion and Facial Expression Detector/creating_model.py
@@ -0,0 +1,226 @@
+import keras
+from keras.preprocessing.image import ImageDataGenerator
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, Activation, Flatten, BatchNormalization
+from keras.layers import Conv2D, MaxPooling2D
+import os
+
+
+
+num_classes = 6
+img_rows, img_cols = 48, 48
+batch_size = 16
+
+train_data_dir = "/home/sky/Documents/2.CNN/Emotion and Facial Expression Detector/fer2013/train"
+validation_data_dir = "/home/sky/Documents/2.CNN/Emotion and Facial Expression Detector/fer2013/validation"
+
+
+#lets use some data augmentation
+
+train_datgen = ImageDataGenerator(rescale=1./255,
+                                  rotation_range=30,
+                                  shear_range=0.3,
+                                  zoom_range=0.3,
+                                  width_shift_range=0.4,
+                                  height_shift_range=0.4,
+                                  horizontal_flip=True,
+                                  fill_mode='nearest')
+
+validation_datagen = ImageDataGenerator(rescale=1./255)
+
+
+train_generator = train_datgen.flow_from_directory(train_data_dir,
+                                                   color_mode='grayscale',
+                                                   target_size=(img_rows,img_cols),
+                                                   batch_size=batch_size,
+                                                   class_mode='categorical',
+                                                   shuffle=True)
+
+
+validation_generator = validation_datagen.flow_from_directory(validation_data_dir,
+                                                   color_mode='grayscale',
+                                                   target_size=(img_rows,img_cols),
+                                                   batch_size=batch_size,
+                                                   class_mode='categorical',
+                                                   shuffle=True)
+
+
+# model like littlwVGG 
+
+model = Sequential()
+
+model.add(Conv2D(32,(3,3), padding='same', kernel_initializer='he_normal',
+                 input_shape=(img_rows,img_cols,1)))
+model.add(Activation('elu'))
+model.add(BatchNormalization())
+model.add(Conv2D(32,(3,3), padding='same', kernel_initializer='he_normal',
+                 input_shape=(img_rows,img_cols,1)))
+model.add(Activation('elu'))
+model.add(BatchNormalization())
+model.add(MaxPooling2D(pool_size=(2,2)))
+model.add(Dropout(0.2))
+
+
+# Block #2: second CONV -> ELU -> CONV -> RELU -> POOL
+# layer set
+model.add(Conv2D(64,(3,3), padding='same', kernel_initializer='he_normal'))
+model.add(Activation('elu'))
+model.add(BatchNormalization())
+model.add(Conv2D(64,(3,3), padding='same', kernel_initializer='he_normal'))
+model.add(Activation('elu'))
+model.add(BatchNormalization())
+model.add(MaxPooling2D(pool_size=(2,2)))
+model.add(Dropout(0.2))
+
+
+# Block #3: third CONV -> ELU -> CONV -> RELU -> POOL
+# layer set
+model.add(Conv2D(128,(3,3), padding='same', kernel_initializer='he_normal'))
+model.add(Activation('elu'))
+model.add(BatchNormalization())
+model.add(Conv2D(128,(3,3), padding='same', kernel_initializer='he_normal'))
+model.add(Activation('elu'))
+model.add(BatchNormalization())
+model.add(MaxPooling2D(pool_size=(2,2)))
+model.add(Dropout(0.2))
+
+
+# Block #4: fourth CONV -> ELU -> CONV -> RELU -> POOL
+# layer set
+model.add(Conv2D(256,(3,3), padding='same', kernel_initializer='he_normal'))
+model.add(Activation('elu'))
+model.add(BatchNormalization())
+model.add(Conv2D(256,(3,3), padding='same', kernel_initializer='he_normal'))
+model.add(Activation('elu'))
+model.add(BatchNormalization())
+model.add(MaxPooling2D(pool_size=(2,2)))
+model.add(Dropout(0.5))
+
+
+# Block #5:  first set of FC -> ELU layers
+model.add(Flatten())
+model.add(Dense(64, kernel_initializer='he_normal'))
+model.add(Activation('elu'))
+model.add(BatchNormalization())
+model.add(Dropout(0.5))
+
+
+# Block 6: second set of Fc -> ELu layes
+model.add(Dense(64, kernel_initializer='he_normal'))
+model.add(Activation('elu'))
+model.add(BatchNormalization())
+model.add(Dropout(0.5))
+
+
+# Block 7: softmax classifier
+model.add(Dense(num_classes, kernel_initializer='he_normal'))
+model.add(Activation('softmax'))
+
+print(model.summary())
+
+
+
+
+from keras.optimizers import Adam
+from keras.callbacks import ModelCheckpoint, EarlyStopping, ReduceLROnPlateau
+
+
+checkpoint = ModelCheckpoint("/home/sky/Documents/h5_model/emo_det.h5",
+                             monitor='val_loss',
+                             mode='min',
+                             save_best_only=True,
+                             verbose=1)
+
+earlystop = EarlyStopping(monitor='val_loss',
+                          min_delta=0,
+                          patience=3,
+                          verbose=1,
+                          restore_best_weights=True)
+
+reduce_lr = ReduceLROnPlateau(monitor='val_loss',
+                              factor=0.2,
+                              patience=3,
+                              verbose=2,
+                              min_delta=0.0001)
+
+
+
+#we put our callbacks into a callback list
+callbacks = [earlystop, checkpoint, reduce_lr]
+
+
+#we use a very small learning rate
+model.compile(loss='categorical_crossentropy',
+              optimizer= Adam(lr=0.001),
+              metrics=['accuracy'])
+
+
+nb_train_samples = 28273
+nb_validation_samples = 3534
+
+epochs = 1
+
+
+history = model.fit_generator(train_generator, epochs=epochs,
+                              steps_per_epoch=nb_train_samples // batch_size,
+                              callbacks=callbacks,
+                              validation_data = validation_generator,
+                              validation_steps = nb_validation_samples // batch_size)
+
+
+
+
+
+
+import matplotlib.pyplot as plt
+import sklearn
+from sklearn.metrics import  classification_report, confusion_matrix
+import numpy as np
+
+
+# we need to recreate our validation generator with shuffle=False
+
+
+validation_generator_s_f = validation_datagen.flow_from_directory(validation_data_dir,
+                                                   color_mode='grayscale',
+                                                   target_size=(img_rows,img_cols),
+                                                   batch_size=batch_size,
+                                                   class_mode='categorical',
+                                                   shuffle=False)
+
+
+class_labels = validation_generator.class_indices
+class_labels = {v: k for k, v in  class_labels.items()}
+classes = list(class_labels.values())
+
+
+#confusion matrix and classification_report
+
+Y_pred = model.predict_generator(validation_generator_s_f, nb_validation_samples // batch_size+1)
+y_pred = np.argmax(Y_pred, axis=1)
+
+
+print("Confusion matrix")
+print(confusion_matrix(validation_generator_s_f.classes,y_pred))
+
+
+print("Classification_report")
+target_names = list(class_labels.values())
+print(classification_report(validation_generator_s_f.classes,y_pred,target_names=target_names))
+
+
+
+plt.figure(figsize = (8,8))
+cnf_matrix = confusion_matrix(validation_generator_s_f.classes,y_pred)
+
+plt.imshow(cnf_matrix, interpolation='nearest')
+plt.colorbar()
+tick_marks = np.arange(len(classes))
+
+_ = plt.xticks(tick_marks, classes, rotation=90)
+_ = plt.yticks(tick_marks, classes)
+
+
+
+
+
---
+++ Emotion and Facial Expression Detector/making_prediction.py
@@ -0,0 +1,91 @@
+## Lets test on some images
+
+from keras.models import load_model
+import cv2
+import re
+import os
+from os import listdir
+from os.path import isfile, join
+from keras.preprocessing import image
+import numpy as np
+from keras.preprocessing.image import  ImageDataGenerator
+
+model = load_model("/home/sky/Documents/h5_model/emo_det.h5")
+
+
+num_classes = 6
+img_width, img_height = 48, 48
+batch_size = 16
+
+
+validation_data_dir = "/home/sky/Documents/2.CNN/Emotion and Facial Expression Detector/fer2013/validation"
+
+validation_datagen = ImageDataGenerator(rescale=1./255)
+
+validation_generator = validation_datagen.flow_from_directory(validation_data_dir,
+                                                   color_mode='grayscale',
+                                                   target_size=(img_width,img_height),
+                                                   batch_size=12,
+                                                   class_mode='categorical',
+                                                   shuffle=False)
+
+
+class_labels = validation_generator.class_indices
+class_labels = {v: k for k, v in  class_labels.items()}
+classes = list(class_labels.values())
+
+
+def draw_test(name, pred, im, true_label):
+    BLACK = [0,0,0]
+    expanded_image = cv2.copyMakeBorder(im, 160, 0, 0, 300, cv2.BORDER_CONSTANT, value=BLACK)
+    cv2.putText(expanded_image, "predicted -"+ pred, (20,60), cv2.FONT_HERSHEY_SIMPLEX, 1, (0,0,255),2)
+    cv2.putText(expanded_image, "true -"+ true_label, (20,120), cv2.FONT_HERSHEY_SIMPLEX, 1, (0,255,0),2)
+    cv2.imshow(name, expanded_image)
+
+
+def getRandomImage(path, img_width, img_height):
+    # function loads a random images from a random folder in our path
+    
+    folders = list(filter(lambda x: os.path.isdir(os.path.join(path, x)), os.listdir(path)))
+    random_directory = np.random.randint(0, len(folders))
+    path_class = folders[random_directory]
+    file_path = path + path_class
+    file_names = [f for f in listdir(file_path) if isfile(join(file_path, f))]
+    random_file_index = np.random.randint(0, len(file_names))
+    image_name = file_names[random_file_index]
+    final_path = file_path+"/"+image_name
+    
+    return image.load_img(final_path, target_size=(img_width, img_height), grayscale=True),final_path,path_class
+
+
+
+files = []
+predictions = []
+true_labels =[]
+
+
+#predicting images
+for i in range(0,10):
+    path = "/home/sky/Documents/2.CNN/Emotion and Facial Expression Detector/fer2013/validation/"
+    img, final_path, true_label = getRandomImage(path, img_width, img_height)
+    files.append(final_path)
+    true_labels.append(true_label)
+    
+    x = image.img_to_array(img)
+    x = x * 1./255
+    x = np.expand_dims(x, axis=0)
+    
+    images = np.vstack([x])
+    classes = model.predict_classes(images,batch_size=10)
+    predictions.append(classes)
+    
+
+
+
+
+
+
+
+
+
+
